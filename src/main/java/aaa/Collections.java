f34246a8d8720cc2b2e5effaff531e681b73bead
GulsumSevilYavuz <gulsumyavuz54@gmail.com>
2023-02-12 21:28:06 +0100

    proje test yenilendi.

---
+++ src/main/java/aaa/Collections.java
@@ -0,0 +1,23 @@
+package aaa;
+
+public class Collections {
+
+    public static void main(String[] args) {
+        int a=12;
+        int b=4;
+        int c[]={3,6,9,12};
+       m(c,a,b);
+    }
+    public static void m(int c[],int a, int b) {
+        try {
+            int idx = a / b;
+            int element = c[idx];
+            System.out.println(element);
+
+
+        } catch (ArithmeticException e) {
+            System.out.println(e.getMessage());
+        }finally {
+            System.out.println("cut the connection with the datebase");
+        }
+    }}
